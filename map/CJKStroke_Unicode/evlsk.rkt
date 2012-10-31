fd8be830529d9ca240dbd2ff2e078818b4bfe25d
fanchylee <li380157578@sina.cn>
2012-11-01 01:49:03 +0800

    stable release 0.1 to be used with ikarus scheme

--- map/CJKStroke_Unicode/evlsk.rkt
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env racket 
-#lang racket
-
-(require "./stroke.scm")
-(define rpl (lambda () 
-	(let ([str (format "~a" (selectsk (read)))])
-		(printf "~a\n" (substring str 1 (- (string-length str) 1)))
-		(rpl))))
-(rpl)
